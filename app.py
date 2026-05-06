164b04d411c986062662f2e0fffd2b6bb0bde94e
danieldabbah12 <dabbah98@gmail.com>
2026-05-06 17:27:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ st.info("""
 - מכירות לפי מוצר
 - ציונים ממוצעים לפי קבוצות
 """)
-st.bar_chart(data=df, *, x="gender", y=None, x_label=None, y_label=None, color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
+st.bar_chart(data=df, x="gender", y=None, x_label=None, y_label=None, color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
