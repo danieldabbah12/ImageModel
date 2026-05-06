832b1be7b51611b1905691b4d2fc082ba72f3ff6
danieldabbah12 <dabbah98@gmail.com>
2026-05-06 17:31:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ df = generate_dataset()
 st.dataframe(df)
 
 
-st.line_chart(data=df, x=None, y=None, x_label=None, y_label=None, color=None, width="stretch", height="content", use_container_width=None)
+st.line_chart(data=df["signal_strength"], x=None, y=None, x_label=None, y_label=None, color=None, width="stretch", height="content", use_container_width=None)
